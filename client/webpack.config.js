6f3129c751b03eb9f589d50a7eac8a30d2aac4c1
CNRBanks <connortamminen@gmail.com>
2022-11-06 10:51:35 -0600

    started working on the webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,11 +18,39 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        display: 'standalone',
+        background_color: '#00001',
+        theme_color: '#00001',
+        start_url: '/',
+        publicPath: '/',
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
 
     module: {
       rules: [
+        {
+          test: 
+        }
         
       ],
     },
